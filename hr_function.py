40300ec7d32a5e72817a629fca24bc9cb2e3a861
G Pooja <pooja.g7277@gmail.com>
2025-12-19 22:56:43 +0530

    HackerRank function

---
+++ hr_function.py
@@ -0,0 +1,9 @@
+#BASIC hackerRank function
+if __name__ == '__main__':
+    n = int(input())
+    def printing(n):
+        for ch in range(1,n+1):
+            print(ch,end="")
+        
+        
+    printing(n)
