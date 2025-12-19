8a160d63831b8dca1fac9132117ba745e9966261
G Pooja <pooja.g7277@gmail.com>
2025-12-19 22:59:51 +0530

    Delete hr_function.py

--- hr_function.py
+++
@@ -1,9 +0,0 @@
-#BASIC hackerRank function
-if __name__ == '__main__':
-    n = int(input())
-    def printing(n):
-        for ch in range(1,n+1):
-            print(ch,end="")
-        
-        
-    printing(n)
