9c68316f27317c557b86227af3ecf928d98ee38f
G Pooja <pooja.g7277@gmail.com>
2025-12-20 21:54:22 +0530

    HackerRank sWAPcASE

---
+++ hr_swapcase.py
@@ -0,0 +1,10 @@
+#swaping of alphabets using swapcase
+#Www.HackerRank.com → wWW.hACKERrANK.COM
+#Pythonist 2 → pYTHONIST 2  
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
